04d714fd749a6bf3091fa05d9e1964fa2af4c184
Calvin Collins <calvinc@surveymonkey.com>
2018-09-11 07:31:07 -0400

    fix install

--- scripts/2.1_install.sh
+++ scripts/2.1_install.sh
@@ -1,4 +1,12 @@
-sudo pacman -S fd bat jq percol udisks2 udiskie ntfs-3g xorg xorg-xinit i3 git feh tmux rofi ranger git python python-pip python2 python2-pip go ruby openssh openssl arandr ttf-dejavu ttf-liberation wqy-zenhei noto-fonts mlocate ripgrep playerctl sysstat vlc docker qt4 dhclient libmariadbclient dunst p7zip unrar perl-file-mimeinfo libreoffice-still thunar zathura zathura-pdf-poppler xss-lock xdg-user-dirs acpid simplescreenrecorder powertop kitty xfce4-power-manager cups system-config-printer openresolv
+sudo pacman -S fd bat jq percol udisks2 udiskie ntfs-3g
+sudo pacman -S git python python-pip python2 python2-pip go ruby
+sudo pacman -S xorg xorg-xinit i3 feh tmux rofi ranger arandr xss-lock dunst powertop
+sudo pacman -S openssh openssl openresolv
+sudo pacman -S playerctl mlocate ripgrep
+sudo pacman -S ttf-dejavu ttf-liberation wqy-zenhei noto-fonts
+sudo pacman -S sysstat vlc docker qt4 dhclient libmariadbclient kitty
+sudo pacman -S p7zip unrar perl-file-mimeinfo libreoffice-still thunar zathura zathura-pdf-poppler
+sudo pacman -S xdg-user-dirs acpid simplescreenrecorder xfce4-power-manager cups system-config-printer
 
 git clone https://aur.archlinux.org/cower.git
 git clone https://aur.archlinux.org/pacaur.git
@@ -8,7 +16,7 @@ makepkg -si
 cd ../pacaur
 makepkg -si
 
-pacaur -S google-chrome nerd-fonts-complete ttf-google-fonts-git laptop-mode-tools slack-desktop zsh-pure-prompt spotify dropbox autorandr alarm-clock-applet gst-plugins-good thunderbird zeal i3lock-next-git
+pacaur -S google-chrome firefox-developer-edition nerd-fonts-complete ttf-google-fonts-git laptop-mode-tools slack-desktop zsh-pure-prompt spotify dropbox autorandr alarm-clock-applet gst-plugins-good thunderbird zeal
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
