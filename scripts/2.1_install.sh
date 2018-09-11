e246b672836a721e7eb5b90a38680119973dc423
Calvin Collins <calvinc@surveymonkey.com>
2018-09-11 07:50:06 -0400

    separate pacaur

--- scripts/2.1_install.sh
+++ scripts/2.1_install.sh
@@ -16,7 +16,9 @@ makepkg -si
 cd ../pacaur
 makepkg -si
 
-pacaur -S google-chrome firefox-developer-edition nerd-fonts-complete ttf-google-fonts-git laptop-mode-tools slack-desktop zsh-pure-prompt spotify dropbox autorandr alarm-clock-applet gst-plugins-good thunderbird zeal
+pacaur -S google-chrome firefox-developer-edition nerd-fonts-complete ttf-google-fonts-git laptop-mode-tools
+pacaur -S slack-desktop zsh-pure-prompt spotify dropbox autorandr alarm-clock-applet gst-plugins-good thunderbird
+pacaur -S zeal
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
