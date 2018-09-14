3e2d988bd7b4867c9f69c7311daf5b0e74033b4a
Calvin Collins <calvinc@surveymonkey.com>
2018-09-14 13:26:27 -0400

    reorder scripts to install everything

--- scripts/2.1_install.sh
+++ scripts/2.1_install.sh
@@ -1,6 +1,7 @@
 sudo pacman -S fd bat jq percol udisks2 udiskie ntfs-3g
 sudo pacman -S git python python-pip python2 python2-pip go ruby
-sudo pacman -S xorg xorg-xinit i3 feh tmux rofi ranger arandr xss-lock dunst powertop
+sudo pacman -S xorg i3
+sudo pacman -S xorg-xinit feh tmux rofi ranger arandr xss-lock dunst powertop
 sudo pacman -S openssh openssl openresolv
 sudo pacman -S playerctl mlocate ripgrep
 sudo pacman -S ttf-dejavu ttf-liberation wqy-zenhei noto-fonts
@@ -19,8 +20,6 @@ makepkg -si
 pacaur -S google-chrome firefox-developer-edition nerd-fonts-complete ttf-google-fonts-git laptop-mode-tools
 pacaur -S slack-desktop zsh-pure-prompt spotify dropbox autorandr alarm-clock-applet gst-plugins-good thunderbird
 pacaur -S zeal
-
-nvm install node
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
