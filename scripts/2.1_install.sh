2ada87636138cb9c52362b8c8b1714aedc26c98d
Calvin Collins <calvinc@surveymonkey.com>
2018-09-17 16:17:47 -0400

    chromium

--- scripts/2.1_install.sh
+++ scripts/2.1_install.sh
@@ -9,18 +9,20 @@ sudo pacman -S ttf-dejavu ttf-liberation wqy-zenhei noto-fonts
 sudo pacman -S sysstat vlc docker qt4 dhclient libmariadbclient kitty
 sudo pacman -S p7zip unrar perl-file-mimeinfo libreoffice-still thunar zathura zathura-pdf-poppler
 sudo pacman -S xdg-user-dirs acpid simplescreenrecorder xfce4-power-manager tlp cups system-config-printer
+sudo pacman -S chromium firefox
 
 git clone https://aur.archlinux.org/cower.git
 git clone https://aur.archlinux.org/pacaur.git
 cd cower
 gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+gpg --recv-keys --keyserver keyserver.ubuntu.com 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 makepkg -si
 cd ../pacaur
 makepkg -si
 
-pacaur -S google-chrome firefox-developer-edition nerd-fonts-complete ttf-google-fonts-git laptop-mode-tools
-pacaur -S slack-desktop zsh-pure-prompt spotify dropbox autorandr alarm-clock-applet gst-plugins-good thunderbird
-pacaur -S zeal betterlockscreen
+pacaur -S nerd-fonts-complete ttf-google-fonts-git laptop-mode-tools
+pacaur -S slack-desktop zsh-pure-prompt spotify-stable dropbox autorandr alarm-clock-applet gst-plugins-good
+pacaur -S zeal betterlockscreen thunderbird
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
