686144109950d0f10920d04d8a2292a35d41c775
Calvin Collins <calvinc@surveymonkey.com>
2018-12-19 10:50:46 -0500

    alacritty

--- scripts/2.1_install.sh
+++ scripts/2.1_install.sh
@@ -7,10 +7,10 @@ sudo pacman -S xorg-xinit feh tmux rofi ranger arandr xss-lock dunst powertop
 sudo pacman -S openssh openssl
 sudo pacman -S playerctl mlocate ripgrep
 sudo pacman -S ttf-dejavu ttf-liberation wqy-zenhei noto-fonts
-sudo pacman -S sysstat vlc docker qt4 dhclient libmariadbclient kitty
+sudo pacman -S sysstat vlc docker qt4 dhclient libmariadbclient alacritty
 sudo pacman -S p7zip unrar perl-file-mimeinfo libreoffice-still thunar zathura zathura-pdf-poppler
 sudo pacman -S xdg-user-dirs acpid simplescreenrecorder xfce4-power-manager tlp cups system-config-printer
-sudo pacman -S chromium pepper-flash firefox
+sudo pacman -S pepper-flash firefox
 sudo pacman -S physlock mysql-workbench
 sudo pacman -S cowsay fortune-mod
 
@@ -21,7 +21,7 @@ cd ..
 
 yay -S nerd-fonts-complete ttf-google-fonts-git laptop-mode-tools
 yay -S slack-desktop spotify-stable dropbox autorandr alarm-clock-applet gst-plugins-good
-yay -S zeal thunderbird chromium-widevine insomnia
+yay -S zeal thunderbird google-chrome insomnia
 
 export NVS_HOME="$HOME/.nvs"
 git clone https://github.com/jasongin/nvs "$NVS_HOME"
