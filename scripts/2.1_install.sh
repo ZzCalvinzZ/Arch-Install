e37629a7596ebfd838d12410cd150bf7cc6932f1
Calvin Collins <calvinc@surveymonkey.com>
2019-05-24 16:18:01 -0400

    couple things

--- scripts/2.1_install.sh
+++ scripts/2.1_install.sh
@@ -6,9 +6,9 @@ sudo pacman -S i3-gaps i3blocks i3status
 sudo pacman -S xorg-xinit feh tmux ranger arandr xss-lock dunst powertop
 sudo pacman -S openssh openssl
 sudo pacman -S playerctl mlocate ripgrep
-sudo pacman -S ttf-dejavu ttf-liberation wqy-zenhei noto-fonts
+sudo pacman -S ttf-dejavu ttf-hack ttf-liberation wqy-zenhei noto-fonts
 sudo pacman -S sysstat vlc docker qt4 dhclient libmariadbclient alacritty kitty
-sudo pacman -S p7zip unrar perl-file-mimeinfo libreoffice-still thunar zathura zathura-pdf-poppler
+sudo pacman -S p7zip unrar perl-file-mimeinfo libreoffice-still
 sudo pacman -S xdg-user-dirs acpid simplescreenrecorder xfce4-power-manager tlp cups system-config-printer
 sudo pacman -S pepper-flash firefox
 sudo pacman -S physlock mysql-workbench
@@ -19,6 +19,7 @@ sudo pacman -S hub
 sudo pacman -S albert
 # sudo pacman -S rofi
 sudo pacman -S pacman-contrib
+sudo pacman -S thunar thunar-archive-plugin xarchiver
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
